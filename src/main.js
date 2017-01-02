ad618613e20c8a5de291e0e00a039d6d215d12dc
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-01-02 07:12:47 +0100

    sxml2str

--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
+
+function sxml2str(o) {
+  if(typeof o === "string") {
+    return document.createTextNode(o);
+  } else if(Array.isArray(o)) {
+    console.log(o[0])
+    let node = document.createElement(o[0]);
+    let tagtype = o[0];
+    let params = o[1];
+    let firstChild;
+    if(typeof params === "object" && params.constructor === Object) {
+      firstChild = 2;
+    } else {
+      params = {};
+      firstChild = 1;
+    }
+    for(let i = firstChild; i < o.length; ++i) {
+      node.appendChild(sxml2str(o[i]));
+    }
+    return node;
+  } else {
+    console.log('err', o, typeof o);
+    throw "unexpected type of parameter to sxml2str - " + o;
+  }
+}
+
+console.log('helo', window.y = sxml2str(
+      ["div",
+      ["div", {id: "a"}, "hello"],
+      ["span", "hello"]]
+      ));
 module.hot && module.hot.accept();
 
 window.x = (window.x || 0) + 1;
