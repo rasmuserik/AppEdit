09357694f3d8f4afaa56941a5ec4b2396359048c
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-01-16 09:57:21 +0100

    use html-text instead of jodom

--- appedit.js
+++ appedit.js
@@ -23,128 +23,42 @@ if(!location.search) {
   location.search = '?Edit';
 }
 var state = {};
-setTimeout(createCodeMirror, 0);
 
 // # Routing
 var route = location.search.slice(1).split('/');
 route[0] = route[0] || 'About';
 
 // # UI
+(()=>{
 document.getElementById('loading').remove();
 (document.getElementById('topbar'+route[0])||{}).className = 'topbar-active';
 
-var menuHeight = '36px';
-var menuBackground = '#345';
-function makeMenuItem(str) {
-
-}
-var rootElem = jsonml2dom(
-    ['div', {id: 'appedit', style:{
-    }},
-
-    // ## Top menu
-
-    ['div', { id: 'topbar', style: {
-      display: 'inline-block',
-      position: 'absolute',
-      top: 0, left: 0, right: 0,
-      height: menuHeight,
-      textAlign: 'left',
-      font: '22px sans-serif',
-      lineHeight: '20px',
-      background: menuBackground,
-      color: '#fff'
-    }},
-    ['img', {src: 'icon.png', width: 36, height: 36, style: {
-      float: 'left'
-    }}]
-    ].concat(
-      ['About', 'Read', 'Edit', 'App', 'Share']
-      .map(s => 
-        ["a", {href: '?'+s, style: {
-          display: 'inline-block',
-          textDecoration: 'none',
-          background: location.search.startsWith('?'+s) ? 
-            '#123' : menuBackground,
-          padding: '8px 10px 8px 10px',
-          color: 'white',
-          fontSize: '16px',
-        }}, s]
-        )
-      ),
-
-    // ## main wrapper
-
-    ['div', {id: 'appedit-main', style: {
-      display: 'inline-block',
-      position: 'absolute',
-      top: menuHeight, left: 0, right: 0 , bottom: 0
-    }}, 
-
-    // ## `Read` the literate code
-
-    location.search.startsWith('?Read') ?
-    ['div', 
-    ['p', 'This is the documentation and source code for the current app. Click on "Edit" or "App" above to run it.'],
-    ['div', 'TODO: documentation / literate source code of current app']]:
-
-    // ## `Edit` the app
-
-    location.search.startsWith('?Edit') ?
-    ['div',
-    ['div', {
-      id: 'appedit-code', 
-      style: {
-        display: 'inline-block',
-        position: 'absolute',
-        overflow: 'auto',
-        top: 0, left: 0, bottom: 0,
-        width: '50%'
-      }}], 
-      ['div', {id: 'appedit-content', style: {
-        display: 'inline-block',
-        position: 'absolute',
-        outline: '1px solid black',
-        overflow: 'auto',
-        top: 0, right: 0, bottom: 0,
-        width: '50%'
-      }}]]:
-
-// ## Run the `App`
-
-location.search.startsWith('?App') ?
-['div', {id: 'appedit-content'}]:
-
-// ## `Share` the app + settings
-
-location.search.startsWith('?Share') ?
-['div', 
-  ['p', 'TODO: sharing-links/buttons, and general settings']]:
-
-  // ## `About` the app editor (default)
-
-  ['div', 
-  'More info to come here...',
-  ['ul',
-  ['li', 'About: this is / made with a tool for live editing small apps'],
-  ['li', 'Introduction: Code App Share(+settings)'],
-  ['li', 'Pricing: currently only free trial, later on plans that allows you to publish the apps outside of this site'],
-  ['li', 'Key bindings during editing'],
-  ['li', 'Introduction to programming + links']
-  ]]]]);
-
-  if(document.getElementById('app')) {
-    document.getElementById('app').remove();
-  }
-//document.body.appendChild(rootElem);
+var app = document.getElementById('app');
+console.log(route[0])
+switch(route[0]) {
+  case 'Read':
+    break;
+  case 'Edit':
+    console.log('here', app);
+    app.innerHTML =
+      '<div id=appedit-code class=main style=right:50%></div>' +
+      '<div id=appedit-content class=main ' +
+      'style="left:50%;outline:1px solid black"></div>';
+    document.getElementById('app')
+    setTimeout(createCodeMirror, 0);
+    break;
+  case 'App':
+    break;
+  case 'Share':
+    break;
+};
+})();
+// # Code editor
 var codemirrorStyle = {
   position: 'absolute',
   top: 0, left: 0,
   width: '100%', height: '100%'
 };
-
-
-// # Code editor
 
 
 if(true|| !localStorage.getItem('appeditContent')) {
