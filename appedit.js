c737d62360b987fb7d9ee74e36608f0b49eb8f7f
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-01-16 10:50:29 +0100

    refactor

--- appedit.js
+++ appedit.js
@@ -1,8 +1,65 @@
 // # Dependencies
 
-var URL = self.URL || self.webkitURL;
-var Worker = self.Worker;
 var da = require('direape');
+var slice = (a, start, end) => Array.prototype.slice.call(a, start, end);
+
+// # UI
+//
+var route = location.search.slice(1).split('/');
+route[0] = route[0] || 'About';
+
+document.getElementById('topbar'+route[0]).className = 'topbar-active';
+
+({
+  About: (o=>o), 
+  Read: read, 
+  Edit: edit, 
+  App: app, 
+  Share: share
+})[route[0]]();
+
+  document.getElementById('loading').remove();
+
+// # Read
+//
+function js2markdown(src) {
+  return ('\n'+src).replace(/\n/g, '\n    ').replace(/\n *[/][/] ?/g, '\n');
+}
+function markdown2html(markdown) {
+  return (new (require('showdown@1.6.0')).Converter()).makeHtml(markdown);
+}
+function read() {
+  var code = localStorage.getItem('appeditContent');
+  document.getElementById('app').innerHTML = markdown2html(js2markdown(code));
+}
+
+// # App
+//
+function app() {
+da.handle('appedit:html', (state, html) => {
+  document.getElementById('app').innerHTML = 
+     '<div id=appedit-content class=main></div>';
+  document.getElementById('appedit-content').innerHTML = html;
+});
+}
+
+// # Share
+//
+function share() {
+  document.getElementById('app').innerHTML = markdown2html(`
+
+# Share 
+
+     options to share on github
+
+      ...not implemented yet...
+      
+    `.replace(/\n */g, '\n'));
+}
+
+// # Editor
+//
+function edit() {
 var CodeMirror = require('codemirror/lib/codemirror');
 require('codemirror/addon/runmode/runmode.js');
 require('codemirror/addon/runmode/colorize.js');
@@ -12,56 +69,19 @@ require('codemirror/addon/fold/markdown-fold.js');
 require('codemirror/keymap/vim.js');
 require('codemirror/mode/javascript/javascript.js');
 require('codemirror/mode/markdown/markdown.js');
-var jsonml2dom = require('./jodom.js').jsonml2dom;
-var setTimeout = self.setTimeout;
-var slice = (a, start, end) => Array.prototype.slice.call(a, start, end);
-var workerPid;
 
-// # Initialisation
-
-if(!location.search) {
-  location.search = '?Edit';
-}
-var state = {};
-
-// # Routing
-var route = location.search.slice(1).split('/');
-route[0] = route[0] || 'About';
-
-// # UI
-(()=>{
-document.getElementById('loading').remove();
-(document.getElementById('topbar'+route[0])||{}).className = 'topbar-active';
-
-var app = document.getElementById('app');
-console.log(route[0])
-switch(route[0]) {
-  case 'Read':
-    break;
-  case 'Edit':
-    console.log('here', app);
-    app.innerHTML =
+    document.getElementById('app').innerHTML =
       '<div id=appedit-code class=main style=right:50%></div>' +
       '<div id=appedit-content class=main ' +
       'style="left:50%;outline:1px solid black"></div>';
-    document.getElementById('app')
-    setTimeout(createCodeMirror, 0);
-    break;
-  case 'App':
-    break;
-  case 'Share':
-    break;
-};
-})();
-// # Code editor
+
 var codemirrorStyle = {
   position: 'absolute',
   top: 0, left: 0,
   width: '100%', height: '100%'
 };
 
-
-if(true|| !localStorage.getItem('appeditContent')) {
+if(!localStorage.getItem('appeditContent')) {
   localStorage.setItem('appeditContent', 
       "var da = require('direape');\n" +
       "da.dispatch(da.msg(da.parent, 'appedit:html',`\n" +
@@ -75,8 +95,10 @@ if(true|| !localStorage.getItem('appeditContent')) {
       "</center>\n" +
       "`));" );
 }
+
+var codemirror;
 function createCodeMirror() {
-  state.codemirror = CodeMirror(
+  codemirror = CodeMirror(
       function(cmElement) {
         cmElement.id = "codemirror";
         Object.assign(cmElement.style, codemirrorStyle);
@@ -90,11 +112,22 @@ function createCodeMirror() {
         lineNumbers: true,
         value: localStorage.getItem('appeditContent')
       });
-  state.codemirror.on('change', function(o) { state.onsourcechange(o); });
+  codemirror.on('change', function(o) { 
+  var content = o.getValue();
+  localStorage.setItem("appeditContent", content);
+  workerExec(content);
+  });
+}
+
+da.handle('appedit:html', (state, html) => {
+  document.getElementById('appedit-content').innerHTML = html;
+});
+    setTimeout(createCodeMirror, 0);
 }
 
 // # Webworker setup
 
+var workerPid;
 function worker() {
   da.dispatch(da.msg.apply(null, [workerPid].concat(slice(arguments))));
 }
@@ -115,68 +148,3 @@ function workerExec(str) {
   worker('reun:run', str, location.href);
 }
 // TODO ping/keepalive
-
-/*
-// ## Handle messages from worker to main thread
-
-function handleWorkerMessage(msg) {
-var o = msg.data;
-switch(o.type) {
-case "ping":
-state.lastPing = Date.now();
-break;
-case "html":
-var html = o.data;
-if(!Array.isArray(o.data)) {
-html = ["div", "Error: html from worker is not JsonML",
-["pre", JSON.stringify(o.data, null, 4)]];
-} 
-var baseElem = document.getElementById("workerHTML");
-if(!baseElem) {
-baseElem = jsonml2dom(
-["div", {
-id: "workerHTML",
-style: { }
-}]);
-document.getElementById('appedit-content').appendChild(baseElem);
-}
-if(baseElem.children[0]) {
-baseElem.children[0].remove();
-}
-baseElem.appendChild(jsonml2dom(html));
-break;
-default:
-console.log('unhandled worker message', o);
-}
-}
-
-state.lastPing = Date.now();
-var silentTime;
-setInterval(function pinger() {
-silentTime = Date.now() - state.lastPing;
-if(silentTime > 200000) {
-console.log('worker not answering, restarting');
-state.lastPing = Date.now();
-newWorker();
-}
-workerExec('self.postMessage({type: "ping"})');
-}, 1000);
-
-function workerExec(o) {
-state.worker.postMessage({dst: 'weare.execute', data: [o, location.href]});
-//state.worker.postMessage({type: 'execute', code: o, url: location.href});
-}
-*/
-
-// # onchange
-
-state.onsourcechange = function(o) {
-  var content = o.getValue();
-  localStorage.setItem("appeditContent", content);
-  workerExec(content);
-};
-
-
-da.handle('appedit:html', (state, html) => {
-  document.getElementById('appedit-content').innerHTML = html;
-});
