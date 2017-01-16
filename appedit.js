e1358fd19f2548a9b34a4a24ec6ab3070c8ae1d7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-01-16 10:50:41 +0100

    reindent

--- appedit.js
+++ appedit.js
@@ -18,7 +18,7 @@ document.getElementById('topbar'+route[0]).className = 'topbar-active';
   Share: share
 })[route[0]]();
 
-  document.getElementById('loading').remove();
+document.getElementById('loading').remove();
 
 // # Read
 //
@@ -36,11 +36,11 @@ function read() {
 // # App
 //
 function app() {
-da.handle('appedit:html', (state, html) => {
-  document.getElementById('app').innerHTML = 
-     '<div id=appedit-content class=main></div>';
-  document.getElementById('appedit-content').innerHTML = html;
-});
+  da.handle('appedit:html', (state, html) => {
+    document.getElementById('app').innerHTML = 
+      '<div id=appedit-content class=main></div>';
+    document.getElementById('appedit-content').innerHTML = html;
+  });
 }
 
 // # Share
@@ -50,79 +50,79 @@ function share() {
 
 # Share 
 
-     options to share on github
+      options to share on github
 
       ...not implemented yet...
-      
-    `.replace(/\n */g, '\n'));
+
+      `.replace(/\n */g, '\n'));
 }
 
 // # Editor
 //
 function edit() {
-var CodeMirror = require('codemirror/lib/codemirror');
-require('codemirror/addon/runmode/runmode.js');
-require('codemirror/addon/runmode/colorize.js');
-require('codemirror/addon/fold/foldcode.js');
-require('codemirror/addon/fold/brace-fold.js');
-require('codemirror/addon/fold/markdown-fold.js');
-require('codemirror/keymap/vim.js');
-require('codemirror/mode/javascript/javascript.js');
-require('codemirror/mode/markdown/markdown.js');
+  var CodeMirror = require('codemirror/lib/codemirror');
+  require('codemirror/addon/runmode/runmode.js');
+  require('codemirror/addon/runmode/colorize.js');
+  require('codemirror/addon/fold/foldcode.js');
+  require('codemirror/addon/fold/brace-fold.js');
+  require('codemirror/addon/fold/markdown-fold.js');
+  require('codemirror/keymap/vim.js');
+  require('codemirror/mode/javascript/javascript.js');
+  require('codemirror/mode/markdown/markdown.js');
 
-    document.getElementById('app').innerHTML =
-      '<div id=appedit-code class=main style=right:50%></div>' +
-      '<div id=appedit-content class=main ' +
-      'style="left:50%;outline:1px solid black"></div>';
+  document.getElementById('app').innerHTML =
+    '<div id=appedit-code class=main style=right:50%></div>' +
+    '<div id=appedit-content class=main ' +
+    'style="left:50%;outline:1px solid black"></div>';
 
-var codemirrorStyle = {
-  position: 'absolute',
-  top: 0, left: 0,
-  width: '100%', height: '100%'
-};
+  var codemirrorStyle = {
+    position: 'absolute',
+    top: 0, left: 0,
+    width: '100%', height: '100%'
+  };
 
-if(!localStorage.getItem('appeditContent')) {
-  localStorage.setItem('appeditContent', 
-      "var da = require('direape');\n" +
-      "da.dispatch(da.msg(da.parent, 'appedit:html',`\n" +
-      "<center>\n" +
-      "  <h1>Change me</h1>\n" +
-      "  <p>Try to edit the code.</p>\n" +
-      "  <p>Choose Edit above, and then<br>\n" +
-      "     alter the code on the left side...</p>\n" +
-      "  (vi keybindings is enabled,<br>\n" +
-      "  so press <tt>i</tt> to insert)\n" +
-      "</center>\n" +
-      "`));" );
-}
+  if(!localStorage.getItem('appeditContent')) {
+    localStorage.setItem('appeditContent', 
+        "var da = require('direape');\n" +
+        "da.dispatch(da.msg(da.parent, 'appedit:html',`\n" +
+        "<center>\n" +
+        "  <h1>Change me</h1>\n" +
+        "  <p>Try to edit the code.</p>\n" +
+        "  <p>Choose Edit above, and then<br>\n" +
+        "     alter the code on the left side...</p>\n" +
+        "  (vi keybindings is enabled,<br>\n" +
+        "  so press <tt>i</tt> to insert)\n" +
+        "</center>\n" +
+        "`));" );
+  }
 
-var codemirror;
-function createCodeMirror() {
-  codemirror = CodeMirror(
-      function(cmElement) {
-        cmElement.id = "codemirror";
-        Object.assign(cmElement.style, codemirrorStyle);
-        document.getElementById('appedit-code').appendChild(cmElement);
-      },
-      {
-        mode: 'javascript',
-        extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
-        lineWrapping: true,
-        keyMap: 'vim',
-        lineNumbers: true,
-        value: localStorage.getItem('appeditContent')
-      });
-  codemirror.on('change', function(o) { 
-  var content = o.getValue();
-  localStorage.setItem("appeditContent", content);
-  workerExec(content);
+  var codemirror;
+  function createCodeMirror() {
+    codemirror = CodeMirror(
+        function(cmElement) {
+          cmElement.id = "codemirror";
+          Object.assign(cmElement.style, codemirrorStyle);
+          document.getElementById('appedit-code').appendChild(cmElement);
+        },
+        {
+          mode: 'javascript',
+          extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
+          lineWrapping: true,
+          keyMap: 'vim',
+          lineNumbers: true,
+          value: localStorage.getItem('appeditContent')
+        });
+    codemirror.on('change', function(o) { 
+      var content = o.getValue();
+      localStorage.setItem("appeditContent", content);
+      workerExec(content);
+    });
+  }
+
+  da.handle('appedit:html', (state, html) => {
+    document.getElementById('appedit-content').innerHTML = html;
   });
-}
-
-da.handle('appedit:html', (state, html) => {
-  document.getElementById('appedit-content').innerHTML = html;
-});
-    setTimeout(createCodeMirror, 0);
+  setTimeout(createCodeMirror, 0);
 }
 
 // # Webworker setup
