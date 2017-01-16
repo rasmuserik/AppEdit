b2f3be52716e3ca533d71a32a16826ebdde1cdfa
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-01-16 06:51:27 +0100

    started moving html into index.html, instead of dynamic generation

--- appedit.js
+++ appedit.js
@@ -25,7 +25,13 @@ if(!location.search) {
 var state = {};
 setTimeout(createCodeMirror, 0);
 
+// # Routing
+var route = location.search.slice(1).split('/');
+route[0] = route[0] || 'About';
+
 // # UI
+document.getElementById('topbar'+route[0]).className = 'topbar-active';
+document.getElementById('loading').remove();
 
 var menuHeight = '36px';
 var menuBackground = '#345';
@@ -130,12 +136,12 @@ location.search.startsWith('?Share') ?
   if(document.getElementById('app')) {
     document.getElementById('app').remove();
   }
-  document.body.appendChild(rootElem);
-  var codemirrorStyle = {
-    position: 'absolute',
-    top: 0, left: 0,
-    width: '100%', height: '100%'
-  };
+//document.body.appendChild(rootElem);
+var codemirrorStyle = {
+  position: 'absolute',
+  top: 0, left: 0,
+  width: '100%', height: '100%'
+};
 
 
 // # Code editor
@@ -186,77 +192,77 @@ function newWorker() {
   }
   da.spawn().then(pid => {
     workerPid = pid;
-      workerExec('require("direape").parent = "' + da.pid + '";');
-      workerExec(localStorage.getItem("appeditContent"));
-    });
-  }
-  newWorker();
-  function workerExec(str) {
-    worker('reun:run', str, location.href);
-  }
-  // TODO ping/keepalive
+    workerExec('require("direape").parent = "' + da.pid + '";');
+    workerExec(localStorage.getItem("appeditContent"));
+  });
+}
+newWorker();
+function workerExec(str) {
+  worker('reun:run', str, location.href);
+}
+// TODO ping/keepalive
 
-  /*
-  // ## Handle messages from worker to main thread
+/*
+// ## Handle messages from worker to main thread
 
-  function handleWorkerMessage(msg) {
-    var o = msg.data;
-    switch(o.type) {
-      case "ping":
-        state.lastPing = Date.now();
-        break;
-      case "html":
-        var html = o.data;
-        if(!Array.isArray(o.data)) {
-          html = ["div", "Error: html from worker is not JsonML",
-            ["pre", JSON.stringify(o.data, null, 4)]];
-        } 
-        var baseElem = document.getElementById("workerHTML");
-        if(!baseElem) {
-          baseElem = jsonml2dom(
-              ["div", {
-                id: "workerHTML",
-                style: { }
-              }]);
-          document.getElementById('appedit-content').appendChild(baseElem);
-        }
-        if(baseElem.children[0]) {
-          baseElem.children[0].remove();
-        }
-        baseElem.appendChild(jsonml2dom(html));
-        break;
-      default:
-        console.log('unhandled worker message', o);
-    }
-  }
+function handleWorkerMessage(msg) {
+var o = msg.data;
+switch(o.type) {
+case "ping":
+state.lastPing = Date.now();
+break;
+case "html":
+var html = o.data;
+if(!Array.isArray(o.data)) {
+html = ["div", "Error: html from worker is not JsonML",
+["pre", JSON.stringify(o.data, null, 4)]];
+} 
+var baseElem = document.getElementById("workerHTML");
+if(!baseElem) {
+baseElem = jsonml2dom(
+["div", {
+id: "workerHTML",
+style: { }
+}]);
+document.getElementById('appedit-content').appendChild(baseElem);
+}
+if(baseElem.children[0]) {
+baseElem.children[0].remove();
+}
+baseElem.appendChild(jsonml2dom(html));
+break;
+default:
+console.log('unhandled worker message', o);
+}
+}
 
-  state.lastPing = Date.now();
-  var silentTime;
-  setInterval(function pinger() {
-    silentTime = Date.now() - state.lastPing;
-    if(silentTime > 200000) {
-      console.log('worker not answering, restarting');
-      state.lastPing = Date.now();
-      newWorker();
-    }
-    workerExec('self.postMessage({type: "ping"})');
-  }, 1000);
+state.lastPing = Date.now();
+var silentTime;
+setInterval(function pinger() {
+silentTime = Date.now() - state.lastPing;
+if(silentTime > 200000) {
+console.log('worker not answering, restarting');
+state.lastPing = Date.now();
+newWorker();
+}
+workerExec('self.postMessage({type: "ping"})');
+}, 1000);
 
-  function workerExec(o) {
-    state.worker.postMessage({dst: 'weare.execute', data: [o, location.href]});
-    //state.worker.postMessage({type: 'execute', code: o, url: location.href});
-  }
-  */
+function workerExec(o) {
+state.worker.postMessage({dst: 'weare.execute', data: [o, location.href]});
+//state.worker.postMessage({type: 'execute', code: o, url: location.href});
+}
+*/
 
-  // # onchange
+// # onchange
 
-  state.onsourcechange = function(o) {
-    var content = o.getValue();
-    localStorage.setItem("appeditContent", content);
-    workerExec(content);
-  };
+state.onsourcechange = function(o) {
+  var content = o.getValue();
+  localStorage.setItem("appeditContent", content);
+  workerExec(content);
+};
 
 
-  da.handle('appedit:html', (state, html) => {
-    document.getElementById('appedit-content').innerHTML = html;
+da.handle('appedit:html', (state, html) => {
+  document.getElementById('appedit-content').innerHTML = html;
 });
