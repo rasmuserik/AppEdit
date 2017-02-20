20e44a271f38ff215108a32784b059bd58c51c0f
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-02-20 11:21:00 +0100

    cleanup + navigation  bar

--- dev.sh
+++ dev.sh
@@ -12,7 +12,7 @@ do
   echo $! > .pid
   cat appedit.js | sed -e 's/^/    /' | sed -e 's/^ *[/][/] \?//' > README.md
   ./node_modules/.bin/eslint appedit.js &
-  ./node_modules/.bin/uglifyjs -c 'pure_funcs=["da.test"]' < appedit.js > appedit.min.js 2> /dev/zero &
+#  ./node_modules/.bin/uglifyjs -c 'pure_funcs=["da.test"]' < appedit.js > appedit.min.js 2> /dev/zero &
   sleep 3
 done
 
